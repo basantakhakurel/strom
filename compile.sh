fbcb5bb4deb521c915c8dcb367b261886584cc0c
basanta33 <basantakhakurel@gmail.com>
2024-09-27 21:50:09 +0200

    reorg for beagle

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 BASE_DIR=$(pwd)
 SRC_DIR="$BASE_DIR/src"
 BUILD_DIR="$BASE_DIR/build"
-INSTALL_DIR="$BASE_DIR/"
+INSTALL_DIR="$BASE_DIR/distr"
 
 # Create the build dir if it does not already exist
 if [ ! -d "$BUILD_DIR" ]; then
---
+++ src/go.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH" ./strom $@
